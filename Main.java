1201f390733ece2f62b2838aa5e09cbbf42b98e3
irfankhan53 <akirfan646@gmail.com>
2026-07-13 17:52:11 +0600

    Create patient create

--- Main.java
+++ Main.java
@@ -2,12 +2,15 @@ import enums.BloodGroup;
 import model.Patient;
 import storage.DataManager;
 import storage.Connect;
+import service.patientService;
+
 
 public class Main{
     public static void main(String[] args) {
         Connect.initialize();
         System.out.println("Hello World!");
         DataManager.loadData();
+        patientService.create();
         DataManager.showData();
     }
 }
--- service/patientService.java
+++ service/patientService.java
@@ -5,16 +5,28 @@ import model.Patient;
 import enums.BloodGroup;
 
 public class patientService {
-    void create(){
+    public static void create(){
+       System.out.println("Enter your name: ");
        String name = Input.scanner.nextLine();
+       System.out.println("Enter your id: ");
        String id = Input.scanner.nextLine();
-       int age = Input.scanner.nextInt();
-       int phone = Input.scanner.nextInt();
+       System.out.println("Enter your age: ");
+       int age = Integer.parseInt(Input.scanner.nextLine());
+       System.out.println("Enter your phone: ");
+       String phone = Input.scanner.nextLine();
+       System.out.println("Enter your gender: ");
        String gender = Input.scanner.nextLine();
+       System.out.println("Enter your symptom: ");
        String symptom = Input.scanner.nextLine();
-       BloodGroup bloodgroup = BloodGroup.AB_NEG;
+       System.out.println("Enter your bloodgroup: ");
+       String blood = Input.scanner.nextLine();
+       BloodGroup bloodgroup = BloodGroup.valueOf(blood);
        
-       Patient patient = new Patient(id,name,age,phone,gender,symptom,BloodGroup bloodgroup);
+       Patient patient = new Patient(id,name,age,phone,gender,symptom, bloodgroup);
+       DataManager.patientList.add(patient);
        
+    }
+    public static void update(){
+        System.out.println("");
     }
 }
